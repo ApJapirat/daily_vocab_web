c3b9f9f02f445131e68ebba275fa510d9b77febe
Apirat Jamsai <proapiratjamsai@gmail.com>
2025-10-25 14:32:16 +0700

    MIYaa

--- src/app/api/word/route.ts
+++ src/app/api/word/route.ts
@@ -4,5 +4,8 @@ import { words } from "@/data/words";
 export async function GET() {
   const randomIndex = Math.floor(Math.random() * words.length);
   const word = words[randomIndex];
-  return NextResponse.json({ word });
+  /// Fetch localhost:8000
+
+  console.log('Tae was here...');  
+  return NextResponse.json({ data: word });
 }
