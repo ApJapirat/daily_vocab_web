197e0c0bab71c012331a103c73dc3f5ac2c8cb12
Apirat Jamsai <proapiratjamsai@gmail.com>
2025-11-08 14:40:38 +0700

    Wed

--- src/app/api/word/route.ts
+++ src/app/api/word/route.ts
@@ -1,11 +1,13 @@
 import { NextResponse } from "next/server";
 import { words } from "@/data/words";
-
+ 
 export async function GET() {
-  const randomIndex = Math.floor(Math.random() * words.length);
-  const word = words[randomIndex];
-  /// Fetch localhost:8000
-
-  console.log('Tae was here...');  
-  return NextResponse.json({ data: word });
+  // const randomIndex = Math.floor(Math.random() * words.length);
+  // const word = words[randomIndex];
+  // fetch localhost:8000
+  const response = await fetch('http://localhost:8000/api/word');
+  console.log(response)
+  const data = await response.json();
+  console.log("Tae was here...")
+  return NextResponse.json({ data });
 }
